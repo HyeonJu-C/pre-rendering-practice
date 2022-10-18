fe2d22cc158836a5290351ee66224329acfeaec4
HyeonJu <hhjc017@gmail.com>
2022-10-18 10:06:31 +0900

    add : filtered events page에 client data fetching 추가

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,26 +1,33 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-
-import { getFilteredEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import ResultsTitle from '../../components/events/results-title';
-import Button from '../../components/ui/button';
-import ErrorAlert from '../../components/ui/error-alert';
+import { Fragment, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import ResultsTitle from '../../components/events/results-title'
+import Button from '../../components/ui/button'
+import ErrorAlert from '../../components/ui/error-alert'
 
 function FilteredEventsPage() {
-  const router = useRouter();
+  const router = useRouter()
+  const [filteredEvents, setFilteredEvents] = useState()
 
-  const filterData = router.query.slug;
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+      })
+      setFilteredEvents(data)
+    }
+    getData()
+  }, [])
 
-  if (!filterData) {
-    return <p className='center'>Loading...</p>;
-  }
+  // events 페이지를 거치지 않고, 바로 filterd events 페이지에 접근하면 filterData = undefined 임
+  const filterData = router.query.slug
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
-
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
 
   if (
     isNaN(numYear) ||
@@ -35,39 +42,38 @@ function FilteredEventsPage() {
         <ErrorAlert>
           <p>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
       </Fragment>
-    );
+    )
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+  if (!filteredEvents) {
+    return <p className="center">loading...</p>
+  }
 
-  if (!filteredEvents || filteredEvents.length === 0) {
+  if (filteredEvents?.length === 0) {
     return (
       <Fragment>
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
       </Fragment>
-    );
+    )
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  const date = new Date(numYear, numMonth - 1)
 
   return (
     <Fragment>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
-  );
+  )
 }
 
-export default FilteredEventsPage;
+export default FilteredEventsPage
