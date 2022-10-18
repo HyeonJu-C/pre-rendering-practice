c25dce7b660441fd6065058d0f394abf43154d10
HyeonJu <hhjc017@gmail.com>
2022-10-18 10:05:05 +0900

    add : all events page에 getStaticProps 추가

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,18 +1,17 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
 
-import { getAllEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import EventsSearch from '../../components/events/events-search';
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
 
-function AllEventsPage() {
-  const router = useRouter();
-  const events = getAllEvents();
+function AllEventsPage({ events }) {
+  const router = useRouter()
 
   function findEventsHandler(year, month) {
-    const fullPath = `/events/${year}/${month}`;
-    
-    router.push(fullPath);
+    const fullPath = `/events/${year}/${month}`
+
+    router.push(fullPath)
   }
 
   return (
@@ -20,7 +19,15 @@ function AllEventsPage() {
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
-  );
+  )
 }
 
-export default AllEventsPage;
+export default AllEventsPage
+
+export async function getStaticProps() {
+  const events = await getAllEvents()
+
+  return {
+    props: { events },
+  }
+}
